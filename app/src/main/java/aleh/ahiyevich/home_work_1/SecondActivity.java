a8e6a5a13d86a4bbe6f4a3cbc802def9bd959e0e
Aleh <aleh2806@yandex.ru>
2022-06-22 19:30:26 +0300

    Initial commit

---
+++ app/src/main/java/aleh/ahiyevich/home_work_1/SecondActivity.java
@@ -0,0 +1,22 @@
+package aleh.ahiyevich.home_work_1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+    public class SecondActivity extends AppCompatActivity {
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_second);
+        }
+
+        public void goBack(View v) {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        }
+    }
+
+
